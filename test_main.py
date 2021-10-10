1f89e7079c10e5530d0d401ea6ca9e0962736f4a
xjg2010go <xjg2010go@163.com>
2021-10-10 08:56:44 +0800

    add webhook

--- test_main.py
+++ test_main.py
@@ -21,4 +21,4 @@ class TestJSONLoaderMethods(unittest.TestCase):
 
 if __name__ == '__main__':
     runner = xmlrunner.XMLTestRunner(output="reports")
-    unittest.main()
+    unittest.main(testRunner=runner)
